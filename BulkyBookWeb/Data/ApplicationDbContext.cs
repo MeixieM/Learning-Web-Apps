421ebb88f265d58383d2c46ed4544691df0f2fb0
Alex Fernan <mercadoalexfernan@gmail.com>
2022-04-23 00:10:17 +0800

    Added DataBase

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) //ctor
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
